767b9fcda2ea99afb39044f37fb34535cb4b2eed
Awa <awamoussasene@gmail.com>
2025-09-19 23:43:23 +0000

    awamouss commit

--- src/main/java/com/ecole221/devOps/Main.java
+++ src/main/java/com/ecole221/devOps/Main.java
@@ -3,11 +3,17 @@ package com.ecole221.devOps;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome! My name's Awa");
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Hello and welcome!");
 
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
 
+        // Boucle infinie pour que Render garde le service en vie
+        while (true) {
+            Thread.sleep(1000);
+        }
     }
+
 }
