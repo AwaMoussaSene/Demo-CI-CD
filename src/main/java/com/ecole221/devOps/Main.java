89d6bf8502fd9e8d30cbad9f0b4d5eefbf80195e
Awa <awamoussasene@gmail.com>
2025-09-20 00:46:23 +0000

    awamoussa sene commit

--- src/main/java/com/ecole221/devOps/Main.java
+++ src/main/java/com/ecole221/devOps/Main.java
@@ -1,13 +1,13 @@
 package com.ecole221.devOps;
 
-import static spark.Spark.*;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Démarrer le serveur sur le port 8080
-        port(8080);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome! My name's Awa, student ");
 
-        // Route pour la page principale
-        get("/", (req, res) -> "<h1>Hello World!</h1><p>My name's Awa</p><img src='https://via.placeholder.com/150'>");
+
     }
 }
